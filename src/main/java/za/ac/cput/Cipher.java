cc582edae924f990ed13814a7ad44ee6f4d9701a
ABURGER219014868 <219014868@mycput.ac.za>
2021-03-28 19:22:01 +0200

    Initial commit, create Cipher.java class and CipherTest.java, add author and readme comment, add maven dependency for project.

---
+++ src/main/java/za/ac/cput/Cipher.java
@@ -0,0 +1,19 @@
+package za.ac.cput;
+
+/**
+ * @author Adriaan(Annemie) Burger 219014868
+ */
+
+/*
+READ ME:
+In my elective of blockchain technology I see how cryptography plays a big role.
+One of the simpler encryptions used as an example was the Ceasar cipher – where the alphabet
+is offset by a certain number (that would be your key) for example if we have the
+right offset or shift of 5 – A would be replaced by F . Your text you want to encrypt would be
+something like “Always” to the cypher text of “Fqbfdx”. To demonstrate this, I wanted to build the code.
+When researching this online, however, I found that someone else has already done that in Java.
+This code imports that maven dependency and runs the main functions to show how the cipher would be
+used but also how easily it could be solved.
+ */
+public class Cipher {
+}
