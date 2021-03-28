ed76c3e801af727adbb10f9a2a7eefc14a3d16d5
ABURGER219014868 <219014868@mycput.ac.za>
2021-03-28 19:52:52 +0200

    Create testCeasarEncrypt() and testCeasarDecrypt() with corresponding java class methods. Tests pass. AssertEquals.

--- src/main/java/za/ac/cput/Cipher.java
+++ src/main/java/za/ac/cput/Cipher.java
@@ -1,19 +1,34 @@
 package za.ac.cput;
 
 /**
+ *
  * @author Adriaan(Annemie) Burger 219014868
+ * Cipher.java
+ * ADP3 Assignment1 March/April 2021
  */
 
 /*
 READ ME:
 In my elective of blockchain technology I see how cryptography plays a big role.
 One of the simpler encryptions used as an example was the Ceasar cipher – where the alphabet
-is offset by a certain number (that would be your key) for example if we have the
-right offset or shift of 5 – A would be replaced by F . Your text you want to encrypt would be
-something like “Always” to the cypher text of “Fqbfdx”. To demonstrate this, I wanted to build the code.
+is offset by a certain number (that would be your key). To demonstrate this, I wanted to build the code.
 When researching this online, however, I found that someone else has already done that in Java.
 This code imports that maven dependency and runs the main functions to show how the cipher would be
 used but also how easily it could be solved.
  */
+import com.grayen.encryption.caesar.algorithm.Caesar;
+import com.grayen.encryption.caesar.algorithm.implementation.CaesarFabric;
+
+
 public class Cipher {
+    Caesar encryption = CaesarFabric.getEncryptionSystem();
+
+    public String ceasarEncrypt(String toEncrypt,int offset){
+        return encryption.encrypt(toEncrypt,offset);
+    }
+
+    public String ceasarDecrypt(String toDecrypt, int offset){
+        return encryption.decrypt(toDecrypt,offset);
+    }
+
 }
--- src/test/java/za/ac/cput/CipherTest.java
+++ src/test/java/za/ac/cput/CipherTest.java
@@ -1,12 +1,36 @@
 package za.ac.cput;
 
+import com.grayen.encryption.caesar.algorithm.Caesar;
+import com.grayen.encryption.caesar.algorithm.implementation.CaesarFabric;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CipherTest {
-
+private Cipher cipher;
+private String textToEncrypt = "Always";
+private String textToDecrypt = "Vaqvsh";
     @BeforeEach
     void setUp() {
+
+        this.cipher = new Cipher();
     }
+
+    @Test
+    public void testCeasarEncrypt(){
+        String result = cipher.ceasarEncrypt(textToEncrypt, 5);
+        assertEquals("Vaqvsh",result);
+    }
+    @Test
+    public void testCeasarDecrypt(){
+        String result = cipher.ceasarDecrypt(textToDecrypt, 5);
+        assertEquals(textToEncrypt,result);
+    }
+
+    //@Test @Timeout(1000)
+    //public void testBruteForce(){
+
+   // }
 }
