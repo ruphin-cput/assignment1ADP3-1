34b1873fc8f57e1c144058758249edce4eaaa34e
ruphin-cput <218321392@mycput.ac.za>
2021-04-01 16:43:45 +0200

    Covid 19 Tracker app by Ruphin Bolonda. 3 Classes have been created : Student which is model class, Covid19Tracker as a controller and Covid19TrackerTest

---
+++ src/main/java/za/ac/cput/Covid19Tracker.java
@@ -0,0 +1,26 @@
+package za.ac.cput;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Ruphin Bolonda
+ * Student Number : 218321392.
+ * Description : Controller Class used in my Covid19Tracker App
+ */
+public class Covid19Tracker {
+
+    ArrayList<Student> students=new ArrayList<Student>();
+
+    public  void addStudent(Student student){
+        students.add(student);
+    }
+
+    public boolean isValidEmail(String email){
+        String regex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher m = pattern.matcher(email);
+        return m.matches();
+    }
+}
